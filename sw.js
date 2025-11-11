f949df4c9b990deea7025ddad74374ad6f80ddc2
evaristekab-rgb <evaristekab@gmail.com>
2025-11-10 20:33:48 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE = "proplanner-pro-reminders-v1";
-const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE = "proplanner-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=> self.skipWaiting()));
 });
-self.addEventListener("activate", e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
 self.addEventListener("fetch", e => {
   const url = new URL(e.request.url);
-  if (ASSETS.includes(url.pathname.replace(/.*\//, "./"))) {
+  if(ASSETS.includes(url.pathname.replace(/.*\//, "./"))){
     e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
   }
 });
