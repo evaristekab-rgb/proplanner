36b701355c29a94d4cb7d9de9c905d51b850cbd5
evaristekab-rgb <evaristekab@gmail.com>
2025-11-11 17:20:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
-const CACHE = "proplanner-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=> self.skipWaiting()));
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
-});
-self.addEventListener("fetch", e => {
-  const url = new URL(e.request.url);
-  if(ASSETS.includes(url.pathname.replace(/.*\//, "./"))){
-    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-  }
-});
+
+const CACHE="proplanner-pro-reminders-v4";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener("activate",e=>{ e.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))); await self.clients.claim(); })()); });
+self.addEventListener("fetch",e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
